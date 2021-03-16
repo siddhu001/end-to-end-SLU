83913c414b26ce4e4e7f725777104010eaa2bf37
EC2 Default User <ec2-user@ip-172-31-13-164.us-west-2.compute.internal>
2021-03-16 04:54:04 +0000

    Update script to back-evaluate badly evaluated models

--- run_out_of_domain_experiment_with_seeds_aws.sh
+++ run_out_of_domain_experiment_with_seeds_aws.sh
@@ -7,10 +7,12 @@ else
 fi
 max_iter=$(expr $num_models - 1)
 
-training_fractions=( 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 )
-training_frac_str=( 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 )
 
-seed=1111
+training_fractions=( 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 )
+training_frac_str=( 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 )
+num_models=16
+
+seed=4444
 
 printf "###########\nTraining and testing on original splits\n####\n"
 # Training and testing on original splits.
